be2447299ff02f3f12a312eaa07cbc97e5bb58a8
emrejz <emre.kayacan@msn.com>
2019-01-09 14:12:42 +0300

    indexFactory socket connection added

---
+++ public/javascripts/controller/indexController.js
@@ -0,0 +1,14 @@
+app.controller('indexController',['$scope',"indexFactory",($scope,indexFactory)=>
+{
+    indexFactory.connectSocket("http://localhost:3000",{
+        reconnectionAttempts:3,
+        reconnectionDelay:500
+    }).then((socket)=>{
+        console.log("bağlantı gerçekleşti",socket);
+        
+    })
+    .catch(err=>{
+        console.log(err);
+        
+    })
+}]);
